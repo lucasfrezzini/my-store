c0dadca3ad81fc8098629427e4094e24ca7831d4
lucasfrezzini <luquiitas.f@gmail.com>
2021-10-24 17:13:30 -0300

    Aprendiendo rutas y agregando modulo Faker para API falsa de pruebas

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-console.log('My app');
+const express  = require('express');
+const faker  = require('faker');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+	res.send('Hola mi server en express')
+});
+
+app.get('/nueva-ruta', (req, res) => {
+	res.send('Hola mi server en express2')
+});
+
+app.get('/products-v1', (req, res) => {
+	res.json({
+		name: 'Product 1',
+		price: 2000,
+	})
+});
+
+app.get('/products', (req, res) => {
+	const products = [];
+	for (let index = 0; index < 100; index++) {
+		products.push(
+			{
+				name: faker.commerce.productName(),
+				price: parseInt(faker.commerce.price(), 10),
+				image: faker.image.imageUrl(),
+			}
+		)
+	}
+	res.json(products);
+});
+
+app.get('/products/:id', (req, res) => {
+	const { id } = req.params;
+	res.json(
+		{
+			id,
+			name: 'Product 4',
+			price: 2000
+		}
+	)
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+	const { categoryId, productId } = req.params;
+	res.json(
+		{
+			categoryId,
+			productId,
+		}
+	)
+})
+
+app.listen(port, () => {
+	console.log('Mi port' + port);
+})
